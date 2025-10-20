48ecc1bc082e0297f9825340a9c6974cfe22e463
aydengibon-droid <aydengibon@gmail.com>
2025-10-20 12:36:34 -0400

    Fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ function App() {
   
   return (
     <>
-    <div className ="h-screen w-screen flec justify-center bg-stone-100">
-    <div className ="flex flex-col items-center m-24 w-1xl">
+    <div className ="h-screen w-screen flex justify-center bg-stone-100">
+    <div className ="flex flex-col items-center m-24 w-3xl">
       <div className="text-2xl">To Do List</div>
       <div className="flex w-full justify-between">
         <div>
-          <button className="bg-blue-400  p-2 rounded-full text-white hover:bg-black cursor-pointer">Add Task</button>
-          <select name="Filter" id="Filter list">
+          <button className="bg-blue-400  p-3 rounded-lg text-white hover:bg-black cursor-pointer">Add Task</button>
+        </div>
+        <div>
+          <select name="Filter" id="Filter list"  className="bg-slate-300  p-3 rounded-lg text-black hover:bg-slate-400 cursor-pointer">
             <option value="All">All</option>
             <option value="Done">I SAY YA DONE</option>
             <option value="Uncompleted">Uncompleted</option>
-          </select>
+          </select>          
         </div>
-        <div>filter</div>
       </div>
       {/* end btns */}
       <div>List</div>
